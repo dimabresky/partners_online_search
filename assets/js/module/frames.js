c41c184aa9277597eeb3ccd10cb1a18109f731dc
dimabresky <dimabresky@gmail.com>
2018-01-16 16:05:56 +0300

    bugs fix

--- assets/js/module/frames.js
+++ assets/js/module/frames.js
@@ -266,11 +266,11 @@
                             '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">'
                         ].join("") + [
 
-                            Travelsoft.CSS_URL + "/search-result/styles.min.css?" + Math.random() * 100000
+                            Travelsoft.CSS_URL + "/search-result/styles.min.css"
 
                         ].map(function (href) {
                             return `<link rel="stylesheet" href="${href}">`;
-                        }).join("");
+                        }).join("") + `<style>${Travelsoft.utils.screen(options.mainIframeCss)}</style>`;
 
                     })(),
 
