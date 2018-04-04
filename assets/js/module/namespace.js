455b898b822e3cbf3215c00880a5d492637bb18a
dimabresky <dimabresky@dimabresky-K56CB>
2018-04-04 10:38:19 +0300

    fetch

---
+++ assets/js/module/namespace.js
@@ -0,0 +1,11 @@
+/**
+ * namespace.js
+ * @author dimabresky
+ * @copyright (c) 2017, travelsoft 
+ */
+
+/**
+ * Пространство имен модуля travelsoft.pm
+ * @type Object
+ */
+var Travelsoft = Travelsoft || {};
